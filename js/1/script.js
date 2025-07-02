7ff0471ad71a832f7162ca8042772e0d717c8e52
Alex <alex_pann@mail.ru>
2025-07-02 14:28:25 +0300

    in task 1 size now changes on input

--- js/1/script.js
+++ js/1/script.js
@@ -1,15 +1,4 @@
 generate.onclick = function f() {
-    const w = parseInt(width.value);
-    const h = parseInt(height.value);
-
-    if (isNaN(w) || isNaN(h) || w < 0 || h < 0) {
-        meme.innerText = ":(";
-        alert("Both values must be positive numbers");
-        return;
-    };
-    cube.style.width = `${w}px`;
-    cube.style.height = `${h}px`;
-
     cube.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 
     const memes = ["Yay", "Wow", "Awesome", "Cool", "Neat", "Great", "Splendid", "Very good"];
@@ -17,4 +6,24 @@ generate.onclick = function f() {
     meme.innerText = `${memes[Math.floor(Math.random() * (memes.length))]}`;
 
     return;
+};
+
+w_in.oninput = function width() {
+    const w = parseInt(w_in.value);
+    if (isNaN(w) || w < 0) {
+        meme.innerText = "Both must be positive integers :(";
+        return;
+    }
+    cube.style.width = `${w}px`;
+    meme.innerText = "";
+};
+
+h_in.oninput = function height() {
+    const h = parseInt(h_in.value);
+    if (isNaN(h) || h < 0) {
+        meme.innerText = "Both must be positive integers :(";
+        return;
+    }
+    cube.style.height = `${h}px`;
+    meme.innerText = "";
 };
